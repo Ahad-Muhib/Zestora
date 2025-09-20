58b3e283f1f8d70eb8d6bf51cbe0c178b2f142af
Ahad-Muhib <your_email@example.com>
2025-09-20 13:19:06 +0600

    some more htmls... hijibiji now

--- zestoras/views.py
+++ zestoras/views.py
@@ -4,9 +4,17 @@ from django.shortcuts import render
 
 
 
+def home(request):
+    return render(request, 'home.html') 
+def hometest(request):
+    return render(request, 'hometest.html') 
+
 def login_view(request):
     return render(request, 'login.html')
 
 
 def signup_view(request):
     return render(request, 'signup.html')
+
+def recipe_view(request):
+    return render(request, 'homeper.html')
