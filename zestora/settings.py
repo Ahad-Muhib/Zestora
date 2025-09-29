e430f493245c7fcd926dc0b01dde7f78acf5855b
MinazurRahman <22201208@uap-bd.edu>
2025-09-30 01:25:37 +0600

    Fix CSRF verification error for add recipe form - Added CSRF_TRUSTED_ORIGINS to settings for local development URLs

--- zestora/settings.py
+++ zestora/settings.py
@@ -29,6 +29,14 @@ DEBUG = True
 
 ALLOWED_HOSTS = ['*']  # For development and testing
 
+# CSRF Settings
+CSRF_TRUSTED_ORIGINS = [
+    'http://127.0.0.1:8000',
+    'http://localhost:8000',
+    'http://127.0.0.1:1848',
+    'http://localhost:1848',
+]
+
 
 # Application definition
 
